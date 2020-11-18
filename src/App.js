8d803c280d696ad36fce543d3190704380d19f7a
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-11-18 19:07:39 -0300

    Exercise 01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h1> Pokedex </h1>
+        <Route exact
+          path="/" 
+          render={(props) => 
+            <Pokedex {...props} pokemons={pokemons} />
+          } 
+        />
+      </div>
+    </BrowserRouter>
   );
 }
 
